07364c5617650aaf82c8d28900d4a3e46e7b338e
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2022-07-10 04:00:39 +0000

    Limpieza del main

--- Principal.java
+++ Principal.java
@@ -1,12 +1,6 @@
 import Clases_externas.Persona;
 public class Principal{
     public static void main(String[] args){
-        Persona p1 = new Persona();
-        p1.nombre = "Victor";
-        p1.cedula = "567";
-        p1.superheroe = "Daredevil";
-        p1.sidekick = "Spiderman";
-        System.out.println(p1);
         
     }
 }
