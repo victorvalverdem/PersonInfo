90647a24793a5ddad36f924bb19554377c27ffe1
V. M. Valverde <93843339+victorvalverdem@users.noreply.github.com>
2022-07-10 04:00:01 +0000

    borrado ToString

--- Clases_externas/Persona.java
+++ Clases_externas/Persona.java
@@ -3,11 +3,6 @@ public class Persona{
     public String nombre;
     public String cedula;
     public String superheroe;
-    public String sidekick;
-    public String mascota;
-    public String pareja;
-  
-    public String toString(){
-        return "Datos: "+this.nombre+" "+this.cedula+" y su superhéroe favorito es "+this.superheroe+", mientras que su sidekick preferido es "+this.sidekick;  
-  }
+    
+    
 }
